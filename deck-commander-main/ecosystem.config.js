a038255e5bf3ae75ce67a6bc850726e7bacfa290
Tim Frenkel <timfrenkel123@gmail.com>
2026-05-10 18:09:34 +0200

    Update ecosystem.config.js

--- deck-commander-main/ecosystem.config.js
+++ deck-commander-main/ecosystem.config.js
@@ -1,1 +1,13 @@
 
+module.exports = {
+  apps: [
+    {
+      name: "deck-commander",
+      script: "dist/standalone-server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
